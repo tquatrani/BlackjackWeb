896721c81eee70fc41cd765f281d0ac50ff281b1
Terry Quatrani <tmq@comcast.net>
2013-11-21 16:52:36 -0500

    Initial commit

---
+++ public/application.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$(document).on("click", "#hit_form input", function() {
+	  $.ajax({
+	    type: 'POST',
+	    url: '/game/player/hit'
+	  }).done(function(msg){
+	    $("#game").replaceWith(msg);
+	  });
+	  return false;
+	});
+};
